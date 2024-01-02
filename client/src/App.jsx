2c4e73f3e90ab45572a0c34aa403fdceac79910f
Odiiego <odiegopaula@gmail.com>
2024-01-02 04:23:52 -0300

    Adicionei a lógica para adicionar produtos as listas.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,8 +16,15 @@ function App() {
   async function addList(e) {
     e.preventDefault();
 
+    const form = e.target;
+
     const newList = {
-      name: 'aff',
+      name: form.name.value,
+      theme: form.theme.value,
+      tags:
+        form.tags.value.trim().split(/[, ]+/)[0] === ''
+          ? []
+          : form.tags.value.trim().split(/[, ]+/),
     };
 
     await fetch('http://localhost:5000/', {
@@ -27,14 +34,78 @@ function App() {
       },
       body: JSON.stringify(newList),
     });
+
+    form.name.value = '';
+    form.theme.value = '';
+    form.tags.value = '';
+  }
+
+  async function addProduct(e) {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const newProduct = {
+      name: form.name.value,
+      quantity: form.quantity.value,
+    };
+
+    await fetch(`http://localhost:5000/${form.dataset.id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newProduct),
+    });
+
+    form.name.value = '';
+    form.quantity.value = '';
   }
 
   return (
     <>
-      <button onClick={addList}>Add List</button>
+      <form onSubmit={addList}>
+        <input type="text" required name="name" />
+        <input type="text" name="theme" />
+        <input type="text" name="tags" />
+        <input type="submit" required value="Add List" />
+      </form>
       <ul>
         {lists.map((list) => {
-          return <li key={list._id}>{list.name}</li>;
+          return (
+            <li key={list._id}>
+              <header>
+                <p>
+                  {list.name} - {list.theme} <br />
+                  {list.tags.map((tag) => {
+                    return <span key={tag}>{`${tag} \t`}</span>;
+                  })}
+                </p>
+
+                <form data-id={list._id} onSubmit={addProduct}>
+                  <input type="text" required name="name" />
+                  <input type="number" name="quantity" />
+                  <input type="submit" required value="Add Product" />
+                </form>
+              </header>
+              <ul>
+                {list.productList.map((product) => {
+                  return (
+                    <li key={product._id}>
+                      <header>
+                        {product.quantity} - {product.name}
+                        <form data-id={list._id} onSubmit={addProduct}>
+                          <input type="text" required name="name" />
+                          <input type="number" name="quantity" />
+                          <input type="submit" required value="Add Brand" />
+                        </form>
+                      </header>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          );
         })}
       </ul>
     </>
