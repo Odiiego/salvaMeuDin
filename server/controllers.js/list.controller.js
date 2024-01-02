5fb2412ad4db4a79359e89694dd4e3ab602eadce
Odiiego <odiegopaula@gmail.com>
2024-01-02 12:35:13 -0300

    Adicionei as rotas e controllers para adicionar produtos e marcas.

--- server/controllers.js/list.controller.js
+++ server/controllers.js/list.controller.js
@@ -1,4 +1,6 @@
 const List = require('../models/list.model');
+const Product = require('../models/product.model');
+const Brand = require('../models/brand.model');
 
 const getLists = async (req, res) => {
   try {
@@ -20,4 +22,31 @@ const addList = async (req, res) => {
   }
 };
 
-module.exports = { getLists, addList };
+const addProduct = async (req, res) => {
+  const product = new Product(req.body);
+  const list = await List.findById(req.params.listID);
+  await list.addProduct(product);
+
+  try {
+    const newList = await list.save();
+    res.status(201).json(newList);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const addBrand = async (req, res) => {
+  const brand = new Brand(req.body);
+  const list = await List.findById(req.params.listID);
+  const product = list.productList.id(req.params.productID);
+  await product.addBrand(brand);
+
+  try {
+    const newBrand = await list.save();
+    res.status(201).json(newBrand);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = { getLists, addList, addProduct, addBrand };
--- server/routes/list.route.js
+++ server/routes/list.route.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
-const { getLists, addList } = require('../controllers.js/list.controller');
+const {
+  getLists,
+  addList,
+  addProduct,
+  addBrand,
+} = require('../controllers.js/list.controller');
 
 const router = express.Router();
 
 router.get('/', getLists);
 router.post('/', addList);
+router.post('/:listID', addProduct);
+router.post('/:listID/:productID', addBrand);
 
 module.exports = router;
