e8cc94e83f0692284f5a9c643069a83745743dd5
Diego <odiegodev@gmail.com>
2024-03-13 03:45:01 -0300

    fix: changed collectCoverage to true

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
+  collectCoverage: true,
+  coverageDirectory: "./coverage/",
   testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
