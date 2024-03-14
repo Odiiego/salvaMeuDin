5c02e3cdefd6b6ce949ce9e34314697ff731a159
Diego <odiegodev@gmail.com>
2024-03-14 17:32:08 -0300

    feat: add express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
-class Person {
-  name() {
-    return "Diego";
-  }
-}
+import express from "express";
+import { config } from "dotenv";
 
-export default Person;
+config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
