c8083c52c81dcbe9f091ce9ca0d2b2014aa60ac2
Diego <odiegodev@gmail.com>
2024-03-21 00:17:44 -0300

    fix: add aditional verifications to isListParams function

--- server/src/utils/isListParams.ts
+++ server/src/utils/isListParams.ts
@@ -3,8 +3,8 @@ import { ICreateListParams } from "../controllers/create-list/protocols";
 export default function isListParams(
   list: ICreateListParams,
 ): list is ICreateListParams {
-  const { name, description } = list;
-  if (name && description) {
+  const { name, description, ...rest } = list;
+  if (name && description && Object.keys(rest).length === 0) {
     return true;
   }
   return false;
