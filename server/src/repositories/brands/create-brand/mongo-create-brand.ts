dca46483d3782a461d3ac625d62d37cffddd74b2
Diego <odiegodev@gmail.com>
2024-03-31 16:33:36 -0300

    feat: add MongoCreateBrandRepository

---
+++ server/src/repositories/brands/create-brand/mongo-create-brand.ts
@@ -0,0 +1,21 @@
+import { ObjectId, PushOperator } from "mongodb";
+import {
+  ICreateBrandParams,
+  ICreateBrandRepository,
+} from "../../../controllers/brands/create-brand/protocols";
+import { MongoClient } from "../../../database/mongo";
+import { IBrand } from "../../../models/brand";
+
+export class MongoCreateBrandRepository implements ICreateBrandRepository {
+  async createBrand(id: string, params: ICreateBrandParams): Promise<IBrand> {
+    const brand = { id: new ObjectId().toHexString(), ...params };
+
+    await MongoClient.db
+      .collection("lists")
+      .updateOne({ "content.id": new ObjectId(id) }, {
+        $push: { "content.$.brands": brand },
+      } as unknown as PushOperator<Document>);
+
+    return brand;
+  }
+}
