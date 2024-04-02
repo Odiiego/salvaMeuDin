d0eb848d4db0946ccf715dc5f332864182beafd7
Diego <odiegodev@gmail.com>
2024-04-01 23:44:53 -0300

    feat: add helper function to facilitate fetching an updated brand from mongo database

---
+++ server/src/repositories/brands/helpers.ts
@@ -0,0 +1,24 @@
+import { ObjectId } from "mongodb";
+import { MongoClient } from "../../database/mongo";
+
+export async function fetchMongoBrand(id: string) {
+  const [brand] = await MongoClient.db
+    .collection("lists")
+    .aggregate([
+      {
+        $match: {
+          "content.brands.id": new ObjectId(id),
+        },
+      },
+      { $unwind: "$content" },
+      { $unwind: "$content.brands" },
+      {
+        $match: {
+          "content.brands.id": new ObjectId(id),
+        },
+      },
+    ])
+    .toArray();
+
+  return brand.content.brands;
+}
