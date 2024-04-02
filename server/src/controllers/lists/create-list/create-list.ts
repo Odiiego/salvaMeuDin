0cea3abbdf7d5875902c145821d8169fb59947e6
Diego <odiegodev@gmail.com>
2024-04-02 00:23:17 -0300

    refactor: changed the error messages and variables for consistency

--- server/src/controllers/lists/create-list/create-list.ts
+++ server/src/controllers/lists/create-list/create-list.ts
@@ -15,13 +15,13 @@ export class CreateListController implements IController {
     httpRequest: HttpRequest<ICreateListParams>,
   ): Promise<HttpResponse<IList | string>> {
     try {
-      if (!httpRequest.body) return badRequest("Please specify a body");
+      const body = httpRequest?.body;
 
-      if (!checkCreateListParams(httpRequest.body))
-        return badRequest("Invalid parameter");
+      if (!body) return badRequest("Please specify a body");
+      if (!checkCreateListParams(body)) return badRequest("Invalid parameter");
 
       const list = await this.createListRepository.createList({
-        ...httpRequest.body,
+        ...body,
         content: [],
       });
 
