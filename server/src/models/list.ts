a03fbdeb271a1501aff900059f236a70639a3491
Diego <odiegodev@gmail.com>
2024-04-09 20:27:03 -0300

    refactor: updated list and user models

--- server/src/models/list.ts
+++ server/src/models/list.ts
@@ -2,6 +2,7 @@ import { IProduct } from "./product";
 
 export interface IList {
   id: string;
+  userId: string;
   name: string;
   description: string;
   content: IProduct[];
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,9 +1,6 @@
-import { IList } from "./list";
-
 export interface IUser {
   id: string;
   googleId: string;
   email: string;
   name: string;
-  lists: IList[];
 }
