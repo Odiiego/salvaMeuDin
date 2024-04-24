3cbbd8042797d390ace3bf5a9ae77eaf13810205
Diego <odiegodev@gmail.com>
2024-04-24 03:08:59 -0300

    feat: add updateProduct route

--- server/src/routes/products.ts
+++ server/src/routes/products.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { isAuthenticated, isOwner } from '../middlewares';
-import { addProduct } from '../controllers/products';
+import { addProduct, updateProduct } from '../controllers/products';
 
 export const router = express.Router();
 
 router.post('/:id', isAuthenticated, addProduct);
+router.patch('/:id', isAuthenticated, updateProduct);
