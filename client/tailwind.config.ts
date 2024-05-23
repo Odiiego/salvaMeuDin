68f52da088b2e71d06384618d074226cb5b759ec
Diego <odiegodev@gmail.com>
2024-05-22 22:40:00 -0300

    style: add aquamarine and downriver palettes

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,39 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        firaMono: ['Fira Mono', 'monospace'],
+      },
+      colors: {
+        aquamarine: {
+          '50': '#eefffb',
+          '100': '#c5fff5',
+          '200': '#8bffed',
+          '300': '#3dffe1',
+          '400': '#14edd3',
+          '500': '#00d1ba',
+          '600': '#00a899',
+          '700': '#00857b',
+          '800': '#056a63',
+          '900': '#0a5752',
+          '950': '#003534',
+        },
+        downriver: {
+          '50': '#e9f9ff',
+          '100': '#cef2ff',
+          '200': '#a7eaff',
+          '300': '#6be0ff',
+          '400': '#26caff',
+          '500': '#00a4ff',
+          '600': '#007aff',
+          '700': '#005fff',
+          '800': '#0051e6',
+          '900': '#004ab3',
+          '950': '#00214d',
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
