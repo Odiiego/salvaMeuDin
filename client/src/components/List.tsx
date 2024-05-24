692070367c5e7f18e0c5cce28316751f70d1f6c0
Diego <odiegodev@gmail.com>
2024-05-24 04:45:01 -0300

    style: changed color to improve contrast

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -36,35 +36,43 @@ function List() {
 
   return (
     <>
-      <header className="flex flex-col items-center mb-8">
-        <h1 className="font-bold text-8xl">{list?.name}</h1>
-        <h2 className="font-bold text-3xl mb-4">R$ {formatCurrency(total)}</h2>
+      <header className="flex flex-col items-center mt-6 mb-8">
+        <h1 className="font-bold text-8xl font-sairaStencil text-downriver-950 select-none">
+          {list?.name}
+        </h1>
+        <h2 className="font-bold text-3xl mb-4 font-sairaStencil text-downriver-950 select-none">
+          R$ {formatCurrency(total)}
+        </h2>
         <ProductForm list={{ id: id, products, setProducts }} />
-        <div className="flex gap-1 items-center mt-1">
+        <div className="flex w-[342px] gap-1 items-center mt-1">
           <span
-            className="flex gap-1 items-center group cursor-pointer"
+            className="flex w-[171px] justify-end gap-1 items-center group cursor-pointer"
             onClick={() => setListMode('economia')}
           >
-            <span className={'font-bold text-right text-xs leading-3'}>
+            <span
+              className={
+                'font-bold text-downriver-950 text-right text-sm leading-3'
+              }
+            >
               Priorizar
               <br />
               Economia
             </span>
             <span
-              className={`w-[30px] h-[30px] rounded-full relative inline-flex items-center justify-start overflow-hidden font-medium transition-all group ${
+              className={`w-[30px] h-[30px] rounded-full relative inline-flex items-center justify-start overflow-hidden font-medium transition-all text-downriver-950 group ${
                 listMode === 'economia'
-                  ? 'bg-teal-300 hover:bg-teal-300 focus:bg-teal-300'
-                  : 'bg-white hover:bg-white focus:bg-white'
+                  ? 'bg-downriver-950 hover:bg-downriver-950 focus:bg-downriver-950'
+                  : 'hover:bg-downriver-50 focus:bg-downriver-50'
               }`}
             >
               <span
-                className={`bg-teal-300 w-48 h-48 rounded rotate-[-40deg] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-10 ml-10 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 group-focus:ml-0 group-focus:mb-32 group-focus:translate-x-0`}
+                className={`bg-downriver-950 w-48 h-48 rounded rotate-[-40deg] absolute bottom-0 left-0 -translate-x-full ease-out duration-1000 transition-all translate-y-full mb-0 ml-0 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 group-focus:ml-0 group-focus:mb-32 group-focus:translate-x-0`}
               ></span>
               <BadgeDollarSign
                 className={`absolute w-full transition-colors duration-300 ease-in-out  ${
                   listMode === 'economia'
-                    ? 'text-white'
-                    : 'group-hover:text-white group-focus:text-white'
+                    ? 'text-aquamarine-50'
+                    : 'group-hover:text-aquamarine-50 group-focus:text-aquamarine-50'
                 }`}
                 strokeWidth={1.5}
                 size={30}
@@ -72,33 +80,34 @@ function List() {
             </span>
           </span>
           <span
-            className="flex gap-1 items-center group cursor-pointer"
+            className="flex w-[171px] justify-start gap-1 items-center group cursor-pointer"
             onClick={() => setListMode('oferta')}
           >
             <span
-              className={`w-[30px] h-[30px] rounded-full relative inline-flex items-center justify-start overflow-hidden font-medium transition-all group ${
+              className={`w-[30px] h-[30px] rounded-full relative inline-flex items-center justify-start overflow-hidden font-medium transition-all text-downriver-950 group ${
                 listMode === 'oferta'
-                  ? 'bg-teal-300 hover:bg-teal-300 focus:bg-teal-300'
-                  : 'bg-white hover:bg-white focus:bg-white'
+                  ? 'bg-downriver-950 hover:bg-downriver-950 focus:bg-downriver-950'
+                  : 'hover:bg-downriver-50 focus:bg-downriver-50'
               }`}
             >
               <span
-                className={
-                  'bg-teal-300 w-48 h-48 rounded rotate-[-40deg] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-10 ml-10 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 group-focus:ml-0 group-focus:mb-32 group-focus:translate-x-0'
-                }
+                className={`bg-downriver-950 w-48 h-48 rounded rotate-[-40deg] absolute bottom-0 left-0 -translate-x-full ease-out duration-1000 transition-all translate-y-full mb-0 ml-0 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 group-focus:ml-0 group-focus:mb-32 group-focus:translate-x-0`}
               ></span>
-
               <BadgePercent
                 className={`absolute w-full transition-colors duration-300 ease-in-out  ${
                   listMode === 'oferta'
-                    ? 'text-white'
-                    : 'group-hover:text-white group-focus:text-white'
+                    ? 'text-aquamarine-50'
+                    : 'group-hover:text-aquamarine-50 group-focus:text-aquamarine-50'
                 }`}
                 strokeWidth={1.5}
                 size={30}
               />
             </span>
-            <span className={'font-bold text-left text-xs leading-3'}>
+            <span
+              className={
+                'font-bold text-downriver-950 text-left text-sm leading-3'
+              }
+            >
               Priorizar
               <br />
               Ofertas
