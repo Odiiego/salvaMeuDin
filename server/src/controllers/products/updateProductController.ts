e2ab5090c136c8e06ce444dcd9654a0ddad90921
Diego <odiegodev@gmail.com>
2024-06-29 03:30:44 -0300

    feat: add metrics logic when updating a product

--- server/src/controllers/products/updateProductController.ts
+++ server/src/controllers/products/updateProductController.ts
@@ -15,6 +15,33 @@ export const updateProductController = async (
     const list = user?.lists.id(listId);
     const product = list?.content.id(id);
     if (!product) return res.sendStatus(400);
+    if (product.quantity !== quantity) {
+      let bestCostProjection = { quantity: 0, value: 0 };
+
+      product.brands.map((brand) => {
+        const costProjection =
+          Math.ceil(quantity / brand.quantity!) * brand.price!;
+
+        if (!brand.metrics) return;
+        brand.metrics.costProjection = costProjection;
+
+        bestCostProjection = {
+          quantity:
+            !bestCostProjection.quantity ||
+            bestCostProjection.quantity > costProjection
+              ? brand.quantity!
+              : bestCostProjection.quantity,
+          value:
+            !bestCostProjection.value ||
+            bestCostProjection.value > costProjection
+              ? costProjection
+              : bestCostProjection.value,
+        };
+      });
+
+      if (!product.bestMetrics) return;
+      product.bestMetrics.costProjection = bestCostProjection;
+    }
 
     product.name = name ? name : product.name;
     product.quantity = quantity || quantity === 0 ? quantity : product.quantity;
